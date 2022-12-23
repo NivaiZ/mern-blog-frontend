279e20d0ed98cacac1735ffd8332f10687025f99
ilya <nivaiz54@gmail.com>
2022-12-23 12:20:42 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import Container from "@mui/material/Container";
+import { Routes, Route } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
+import { Header } from "./components";
+import { Home, FullPost, Registration, AddPost, Login } from "./pages";
+import React from "react";
+import { fetchAuthMe, selectIsAuth } from "./redux/slices/auth";
+
+function App() {
+  const dispatch = useDispatch();
+  const IsAuth = useSelector(selectIsAuth);
+  React.useEffect(() => {
+    dispatch(fetchAuthMe())
+  }, [])
+  return (
+    <>
+      <Header />
+      <Container maxWidth="lg">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/posts/:id" element={<FullPost />}></Route>
+          <Route path="/posts/:id/edit" element={<AddPost />} />
+          <Route path="/add-post" element={<AddPost />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Registration />}></Route>
+        </Routes>
+      </Container>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Registration/index.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import Avatar from '@mui/material/Avatar';
+import { useForm } from 'react-hook-form';
+import { Navigate } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRegister, selectIsAuth } from "../../redux/slices/auth";
+import styles from './Login.module.scss';
+
+export const Registration = () => {
+  const isAuth = useSelector(selectIsAuth);
+
+  const dispatch = useDispatch();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    defaultValues: {
+      fullName: 'Вася Пупкин',
+      email: 'vasya@test.ru',
+      password: '12345'
+    },
+    mode: 'onChange'
+  })
+
+  const onSubmit = async (values) => {
+    const data = await dispatch(fetchRegister(values));
+    console.log(data);
+    if (!data.payload) {
+      return alert('Не удалось зарегистрироваться!')
+    }
+
+    if ('token' in data.payload) {
+      window.localStorage.setItem('token', data.payload.token)
+    }
+  }
+  if (isAuth) {
+    return <Navigate to="/" />
+  }
+
+  return (
+    <Paper classes={{ root: styles.root }}>
+      <Typography classes={{ root: styles.title }} variant="h5">
+        Создание аккаунта
+      </Typography>
+      <div className={styles.avatar}>
+        <Avatar sx={{ width: 100, height: 100 }} />
+      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          type="text"
+          className={styles.field}
+          error={Boolean(errors.fullName?.message)}
+          helperText={errors.fullName?.message}
+          {...register('fullName', { required: 'Укажите полное имя' })} fullWidth />
+
+        <TextField
+          type="email"
+          className={styles.field}
+          error={Boolean(errors.email?.message)}
+          helperText={errors.email?.message}
+          {...register('email', { required: 'Укажите почту' })} fullWidth />
+
+        <TextField
+          type="password"
+          error={Boolean(errors.password?.message)}
+          className={styles.field}
+          helperText={errors.password?.message}
+          {...register('password', { required: 'Укажите пароль' })} fullWidth />
+        <Button disabled={!isValid} type="submit" size="large" variant="contained" fullWidth>
+          Зарегистрироваться
+        </Button>
+      </form>
+    </Paper>
+  );
+};
